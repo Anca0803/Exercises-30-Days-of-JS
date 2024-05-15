b2c2cf9c6a0d29f4e5ebee1c16fb5240294008f5
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-15 22:31:45 +0200

    Update ex_Level_2_Day2.js

--- ex_Level_2_Day2.js
+++ ex_Level_2_Day2.js
@@ -42,11 +42,11 @@ searchOnJargon.includes("on");
 
 //Generate a random number between 0 and 100 inclusively.
 
-let randomNum = Math.floor(Math.random()*101);
-console.log(randomNum);
+Math.floor(Math.random()*101);
+
 
 //Generate a random number between 50 and 100 inclusively.
-
+Math.floor(Math.random()*51)+50
 
 //Generate a random number between 0 and 255 inclusively.
 
