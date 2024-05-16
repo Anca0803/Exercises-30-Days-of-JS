c3ceb6734e2b93fef156fccb541f8398290beb3d
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-16 18:01:06 +0200

    Update ex_Level_1_Day3.js

--- ex_Level_1_Day3.js
+++ ex_Level_1_Day3.js
@@ -1,15 +1,36 @@
 
 
    //  Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = "John";
+let lastName = "Morris";
+let country ="USA";
+let city = "Chicago";
+let age = 30;
+let isMarried = true;
+let year = 2024;
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+
 
    // Check if type of '10' is equal to 10
+console.log(typeof "10"==10);
+
+
 
    //   Check if parseInt('9.8') is equal to 10
+
+console.log(parseInt('9.8') == 10);
+
 
     //  Boolean value is either true or false.
         Write three JavaScript statement which provide truthy value.
         Write three JavaScript statement which provide falsy value.
 
+
+           
     //  Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
         4 > 3
         4 >= 3
@@ -24,6 +45,8 @@
         4 === '4'
         Find the length of python and jargon and make a falsy comparison statement.
 
+
+           
    //   Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
         4 > 3 && 10 < 12
         4 > 3 && 10 > 12
