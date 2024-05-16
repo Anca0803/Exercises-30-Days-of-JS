5d8d96ded06e8df6f53af4ba0b88866bb720ef06
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-16 16:01:44 +0200

    Update ex_Level_3_Day2.js

--- ex_Level_3_Day2.js
+++ ex_Level_3_Day2.js
@@ -3,8 +3,15 @@
 Count the number of word love in this sentence. */
 
 
+let string2 ="'Love is the best thing in this world. Some found their love and some are still looking for their love.' "
+let patternTwo = /love/gi;
+console.log(string2.match(patternTwo));
+
 // Use match() to count the number of all because in the following sentence:  'You cannot end a sentence with because because because is a conjunction'
 
+let string3 = "'You cannot end a sentence with because because because is a conjunction'";
+let patternThree = /because/gi;
+console.log(string3.match(patternThree));
 
 
 /* Clean the following text and find the most frequent word (hint, use replace and regular expressions).
