c78b0a148e21f493116992918f685207e17a6781
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-21 15:04:12 +0200

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,3 +1,5 @@
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data type
+
 firstName="Anca";     //string
 2!==="2";               //boolean FALSE
 let name;             // undefined
