b8f89de1fc9d30ebf9132e7ef3b5148774709535
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-21 15:03:54 +0200

    Update ex_Day1.js

--- ex_Day1.js
+++ ex_Day1.js
@@ -11,7 +11,7 @@ easy to reuse and informative */
 
 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
-
+DONE
 
 Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 
