9eab4052aae7e592951f8023e6b915715c8f40da
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-26 22:09:48 +0200

    Update ex_Day4_Level1.js

--- ex_Day4_Level1.js
+++ ex_Day4_Level1.js
@@ -7,7 +7,7 @@ Enter your age:15
 You are left with 3 years to drive.  */
 
 
-  let age = prompt("Enter your age:");
+let age = prompt("Enter your age:");
 if (age >= 18) {
     console.log("You are old enough to drive");
 }
