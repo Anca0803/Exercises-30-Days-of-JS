28e40a5ba0becf9b7d1170aa4424898942b5fb5b
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-27 18:02:56 +0200

    Update ex_Day4_Level3.js

--- ex_Day4_Level3.js
+++ ex_Day4_Level3.js
@@ -11,7 +11,31 @@
   Enter a month: FEbruary
   February has 28 days.  */
 
-    
+  let monthInput =prompt("Enter a month:");
+  let month = monthInput.toLowerCase();
+  
+  switch (month) {
+    case 'january':
+    case 'march':
+    case 'may':
+    case 'july':
+    case 'august':
+    case 'october':   
+    case 'december':
+        console.log(`${month} has 31 days`)
+         break
+    case "april":
+    case "june":
+    case "september":
+    case "november":
+        console.log(`${month} has 30 days`)
+        break;
+    case "febuary":
+        console.log(`${month} has 29 days`)
+         break;
+    default:
+        console.log("please enter a valid month");
+        }  
 
     
 Write a program which tells the number of days in a month, now consider leap year.
