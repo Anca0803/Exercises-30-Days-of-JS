bf95684f6ccf86ea045a69838f306cfea93b8286
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-05-27 18:28:54 +0200

    Update ex_Day4_Level3.js

--- ex_Day4_Level3.js
+++ ex_Day4_Level3.js
@@ -39,3 +39,35 @@
 
     
 Write a program which tells the number of days in a month, now consider leap year.
+
+let month = prompt("Enter a month:");
+let year = parseInt(prompt("Enter a year:"));
+month = month[0].toUpperCase() + month.slice(1).toLowerCase();
+switch (month) {
+  case "January":
+  case "March":
+  case "May":
+  case "July":
+  case "August":
+  case "October":
+  case "December":
+    console.log(`${month} has 31 days in ${year}.`);
+    break;
+  case "April":
+  case "June":
+  case "September":
+  case "November":
+    console.log(`${month} has 30 days in ${year}.`);
+    break;
+  case "February":
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+      console.log(`${month} has 29 days in ${year}.`);
+      break;
+    } else {
+      console.log(`${month} has 28 days in ${year}.`);
+      break;
+    }
+  default:
+    alert("Please enter a valid month name!");
+    break;
+}
