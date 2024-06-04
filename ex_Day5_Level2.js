5aaa5586bd89c51477266c08172cf582eb334293
Anca Irimia <119537835+Anca0803@users.noreply.github.com>
2024-06-04 17:36:09 +0200

    Update ex_Day5_Level2.js

--- ex_Day5_Level2.js
+++ ex_Day5_Level2.js
@@ -12,22 +12,72 @@ console.log(words.length)
 // In the following shopping cart add, remove, edit items
 
 const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
-add 'Meat' in the beginning of your shopping cart if it has not been already added
-add Sugar at the end of you shopping cart if it has not been already added
-remove 'Honey' if you are allergic to honey
-modify Tea to 'Green Tea'
 
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+
+shoppingCart.unshift("Meat");
+
+// add Sugar at the end of you shopping cart if it has not been already added
+
+shoppingCart.push("Sugar");
+
+// remove 'Honey' if you are allergic to honey
+shoppingCart.splice(shoppingCart.indexOf("Honey"), 1);
+
+
+// modify Tea to 'Green Tea'
+
+shoppingCart[shoppingCart.indexOf("Tea")] = "Green Tea";
+console.log(shoppingCart);
 
 
 // In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
 
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'India',
+    'Japan',
+    'Kenya'
+]
+
+countries.includes("Ethiopia") === true
+    ? console.log("Ethiopia")
+    : countries.push("Ethiopia")
+
 // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+webTechs.includes("Sass") === true
+    ? console.log("Sass is a CSS preprocess")
+    : webTechs.push("Sass")
+console.log(webTechs)
+
 
 // Concatenate the following two variables and store it in a fullStack variable.
 
 const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 const backEnd = ['Node','Express', 'MongoDB']
-
+const fullStack = frontEnd.concat(backEnd)
 console.log(fullStack)
 ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+
+
+
 
